b6c073f2fa9b766ed85aa3a131b2c655c3f52f96
Evan Sussman <esussma1@binghamton.edu>
2012-02-20 21:44:40 -0500

    Did FIXME's for Result class
    initalized the arraylist
    made constructor private
    made the method synchronized and it adds an element(not checked yet)
    prints factors (not checked yet).'

--- assignment1/src/toyThreads/primeFactors/Results.java
+++ assignment1/src/toyThreads/primeFactors/Results.java
@@ -1,6 +1,7 @@
 
 package toyThreads.primeFactors;
-
+import java.util.Collections;
+import java.util.List;
 import toyThreads.util.Debug;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -8,17 +9,19 @@ import java.util.Iterator;
 public class Results {
 
     // FIXME: instantiate the arrayList here, instead of setting it to null;
-    private static ArrayList<Integer> factorsFound = null;
+    private static ArrayList<Integer> factorsFound = new ArrayList<Integer>();
 
     // FIXME: change this to a private constructor
-    public Results() {}
+    private Results() {}
 
     // FIXME: make this method thread-safe
-    public static void addFactor(int newFactor) {
-	// FIXME
+    public static synchronized void addFactor(int newFactor) {
+        factorsFound.add(newFactor);
     }
 
     public static void printFactors() {
 	// FIXME: all the factors in the ArrayList should be printed here
+      for(int fact = 0;  fact < factorsFound.size(); fact++)
+        System.out.println(factorsFound.get(fact) + "\n");
     }
 }
