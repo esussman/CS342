33f889af1687dd9cb0f3678eac0aaec0a0848ed0
Evan Sussman <esussma1@binghamton.edu>
2012-04-16 13:39:26 -0400

    enums are gooodyyy

--- Assignment3E/src/customerSearch/util/FileEnums.java
+++ Assignment3E/src/customerSearch/util/FileEnums.java
@@ -2,7 +2,7 @@ package customerSearch.util;
 
 public enum FileEnums
 {
-  Product1("product1.txt"), Product2("product2.txt"), Product3("product3.txt"), Product4("Pr"), Product5("sdasd"), Product6("sdas");
+  Product1("product1.txt"), Product2("product2.txt"), Product3("product3.txt"), Product4("product4.txt"), Product5("product5.txt"), Product6("product6.txt"), Product7("product7.txt"),Product8("product8.txt"),Product9("product9.txt"),Product10("product10.txt"),Product11("product11.txt"),Product12("product12.txt");
 
   private String descriptionLocation;
 
