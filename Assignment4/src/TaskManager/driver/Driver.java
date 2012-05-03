0423bc95590d34f9a782f9d517e9fb5118f8993d
Evan Sussman <esussma1@binghamton.edu>
2012-05-03 12:03:11 -0400

    driver code now tests communication between subject tab and observer display

--- Assignment4/src/TaskManager/driver/Driver.java
+++ Assignment4/src/TaskManager/driver/Driver.java
@@ -1,6 +1,9 @@
 package TaskManager.driver;
 
 import TaskManager.subjectFileReader.SubjectFileReader;
+import TaskManager.display.StandardOut;
+import TaskManager.display.Display;
+import TaskManager.data.Performance;
 import TaskManager.tabs.PerformanceTab;
 import TaskManager.tabs.Tab;
 import java.util.Observer;
@@ -8,10 +11,10 @@ public class Driver
 {
   public static void main(String[] args)
   {
-    SubjectFileReader sfr = new SubjectFileReader();
-    Tab p = new PerformanceTab();
-    sfr.addObserver((Observer)p);
-    sfr.update();
-    sfr.notifyObservers();
+    Performance p = new Performance(25, 20, 20 ,20);
+    PerformanceTab pt = new PerformanceTab();
+    Display sout = new StandardOut();
+    pt.addObserver(sout);
+    pt.changed(p);
   }
 }
