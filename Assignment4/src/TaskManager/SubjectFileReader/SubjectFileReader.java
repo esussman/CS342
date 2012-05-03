645a60b2e0cb86d72bb74c90411a1fdad52c4fbf
Evan Sussman <esussma1@binghamton.edu>
2012-05-03 12:59:57 -0400

    added tabs to subject file reader

--- Assignment4/src/TaskManager/SubjectFileReader/SubjectFileReader.java
+++ Assignment4/src/TaskManager/SubjectFileReader/SubjectFileReader.java
@@ -2,9 +2,46 @@ package TaskManager.subjectFileReader;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class SubjectFileReader extends Observable
 {
+  Display display;
+  PerformanceTab performanceTab;
+  ProcessTab processTab;
+  UserTab userTab;
+
+  public SubjectFileReader(Display d)
+  {
+    this.display = d;
+    performanceTab = new PerformanceTab();
+    performanceTab.addObserver(d);
+    processTab = new ProcessTab();
+    processTab.addObserver(d);
+    userTab = new UserTab();
+    userTab.addObserver(d);
+  }
+  public beginProcess()
+  {
+    Scanner userInput;
+    try {
+      userInput = new Scanner(new File("input.txt"));
+          while (userInput.hasNextLine()) {
+            System.out.println(userInput.nextLine());
+          }
+    }
+    catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    finally
+    {
+
+    }
+  }
   public void update()
   {
     this.setChanged();
